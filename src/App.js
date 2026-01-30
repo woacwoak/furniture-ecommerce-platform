e0d34660bc7a1cccc654eaf8a2dddd5b886bdc69
woacwoak <woacwoak@gmail.com>
2026-01-30 19:23:15 +0100

    Changed css, removed bootstrap, css only

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './Pages/Home';
 import Header from './Components/Header';
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -5,53 +5,58 @@ import CartIcon from '../../assets/shoppingCartIcon.svg'
 import UserIcon from '../../assets/userIcon.svg'
 
 const Header = () => {
-    return(
-        <>
-        <div className="headerWrapper">
-            <div className="top-strip bg-blue">
-                <div className="container">
-                    <p className="mb-0 mt-0 text-center">Зимовий Розпродаж - Знижки до 60%</p>
-                </div>
-            </div>
+  return (
+    <>
+      <div className="top-stripe bg-blue">
+        <div className="container">
+          <p>Зимовий Розпродаж - Знижки до 60%</p>
         </div>
+      </div>
 
-        <div className="header">
-            <div className="container">
-                <div className="row">
-                    <div className="logoWrapper d-flex align-items-center col-sm-2">
-                        <Link to={'/'}><img src={Logo} alt="Logo"/></Link>
-                    </div>
+      <header className="header">
+        <div className="container">
+          <div className="headerInner">
 
-                    <div className="navBar col-sm-5 d-flex justify-content-around align-items-center">
-                        <nav>
-                            <ul className="mb-0">
-                                <li><NavLink to={'/catalog'}>Каталог</NavLink></li>
-                                <li><NavLink to={'/'}>Про магазин</NavLink></li>
-                                <li><NavLink to={'/'}>Доставка та оплата</NavLink></li>
-                                <li><NavLink to={'/'}>Гарантія</NavLink></li>
-                                <li><NavLink to={'/'}>Контакти</NavLink></li>
-                            </ul>
-                        </nav>
-                    </div>
-                    <div className="searchBar col-sm-2 d-flex justify-content-center align-items-center">
-                        <input type="text" className="searchInput" placeholder="Пошук..."/>
-                    </div>
-                    <div className="cartIcon col-sm-1 d-flex justify-content-center align-items-center">
-                        <Link to={'/cart'}>
-                            <i><img src={CartIcon} alt="cart"/></i>
-                            <span className="cartCount">0</span>
-                        </Link>
-                        
-                    </div>
-                    <div className="userOptions col-sm-2 d-flex justify-content-end align-items-center">
-                        <img src={UserIcon} alt="user"/>
-                        <Link to={'/login'} className="loginLink">Увійти</Link>
-                    </div>
-                </div>
+            <div className="headerLeft">
+              <Link to="/">
+                <img src={Logo} alt="Logo" className="logo" />
+              </Link>
             </div>
+
+            <nav className="headerNav">
+              <ul className="navLinks">
+                <li><NavLink to="/catalog">Каталог</NavLink></li>
+                <li><NavLink to="/">Про магазин</NavLink></li>
+                <li><NavLink to="/">Доставка та оплата</NavLink></li>
+                <li><NavLink to="/">Гарантія</NavLink></li>
+                <li><NavLink to="/">Контакти</NavLink></li>
+              </ul>
+            </nav>
+
+            <div className="headerRight">
+              <input
+                type="text"
+                className="searchInput"
+                placeholder="Пошук..."
+              />
+
+              <Link to="/cart" className="cartIcon">
+                <img src={CartIcon} alt="cart" />
+                <span className="cartCount">0</span>
+              </Link>
+
+              <Link to="/login" className="userOption">
+                <img src={UserIcon} alt="user" />
+                <span>Увійти</span>
+              </Link>
+            </div>
+
+          </div>
         </div>
-        </>
-    )
-}
+      </header>
+    </>
+  );
+};
+
 
 export default Header;
